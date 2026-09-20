3f7e9cb95bfbeb0322fea81c85ba14c53dbb7915
Bhanu Prakash Reddy Konepalli <51398646+Bhanu9Prakash@users.noreply.github.com>
2026-09-20 16:59:15 +0530

    Solve dl-perceptron

--- study-plans/cracking-dl/dl-perceptron/dl-perceptron.py
+++ study-plans/cracking-dl/dl-perceptron/dl-perceptron.py
@@ -6,19 +6,15 @@ def perceptron(X: list, y: list, lr: float = 0.1, epochs: int = 100) -> tuple:
     """
     X = np.array(X)
     y = np.array(y)
-    n_features = X.shape[1]
-    W = np.zeros(n_features)
+    W = np.zeros(X.shape[1])
     b = 0
 
     for _ in range(epochs):
-        for xi, yi in zip(X, y):
-            z = xi@W + b
+        for xi, yi_true in zip(X, y):
+            z = xi.T@W + b
             yi_pred = 1 if z >= 0 else 0
-            error = yi - yi_pred
+            error = yi_true - yi_pred
             W = W + lr*error*xi
             b = b + lr*error
-        
-    W = np.round(W, 2)
-    b = np.round(b, 2)
 
     return W.tolist(), b
