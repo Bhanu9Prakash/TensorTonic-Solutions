26e6cfec15923c00da97eed5e7c2ea6ba73b2971
Bhanu Prakash Reddy Konepalli <51398646+Bhanu9Prakash@users.noreply.github.com>
2026-09-22 22:06:32 +0530

    Solve pytorch-manual-weight-update

---
+++ study-plans/pytorch-basics/pytorch-manual-weight-update/pytorch-manual-weight-update.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+
+def manual_train_step(model: nn.Module, X: torch.Tensor, y: torch.Tensor, criterion: nn.Module, lr: float) -> float:
+    """
+    Returns the pre-update batch loss as a Python float.
+    """
+    model.train()
+
+    
+    preds = model(X)
+    loss = criterion(preds, y)
+    loss.backward()
+
+    with torch.no_grad():
+        for param in model.parameters():
+            param -= lr*param.grad
+
+    for param in model.parameters():
+        if param.grad is not None:
+            param = param.grad.zero_()
+        
+    return loss.item()
