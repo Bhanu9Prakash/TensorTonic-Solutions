316828a760846f6c23e4ca753f2d4a83a8efff42
Bhanu Prakash Reddy Konepalli <51398646+Bhanu9Prakash@users.noreply.github.com>
2026-09-26 11:14:14 +0530

    Solve pytorch-residual-block

--- study-plans/pytorch-basics/pytorch-residual-block/pytorch-residual-block.py
+++ study-plans/pytorch-basics/pytorch-residual-block/pytorch-residual-block.py
@@ -4,26 +4,23 @@ import torch.nn as nn
 class ResidualBlock(nn.Module):
     def __init__(self, channels: int):
         super().__init__()
-
-        self.conv1 = nn.Conv2d(channels, channels, 3, padding=1)
+        self.conv1 = nn.Conv2d(channels, channels, 3, padding = 1) 
         self.bn1 = nn.BatchNorm2d(channels)
-
-        self.conv2 = nn.Conv2d(channels, channels, 3, padding=1)
+        self.conv2 = nn.Conv2d(channels, channels, 3, padding = 1)
         self.bn2 = nn.BatchNorm2d(channels)
-
         self.relu = nn.ReLU()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Returns a float32 tensor with the same shape as x.
+        """
         identity = x
-
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
-
         out = self.conv2(out)
         out = self.bn2(out)
-
-        out = out + identity
+        out = identity + out
         out = self.relu(out)
-
+        
         return out
